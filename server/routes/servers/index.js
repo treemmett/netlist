330ea6922ee01cc53389bfa3d6d73e62c0fb987b
treemmett <treemmett@gmail.com>
2018-06-15 23:30:45 -0600

    Corrects schema validation

--- server/routes/servers/index.js
+++ server/routes/servers/index.js
@@ -1,27 +1,35 @@
 const servers = require('express').Router();
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const serverSchema = mongoose.Schema({
   applications: [{type: String}],
-  backupDate: String,
-  cpu: String,
-  disks: String,
-  dnsName: String,
-  maintWin: String,
-  memory: String,
+  backupDate: {type: String, trim: true},
+  cpu: {type: String, trim: true},
+  disks: {type: String, trim: true},
+  dnsName: {type: String, trim: true},
+  maintWin: {type: String, trim: true},
+  memory: {type: String, trim: true},
   monitoring: Boolean,
-  os: String,
-  owner: String,
-  patchDate: String,
-  serverName: {type: String, required: true, unique: true},
-  updatedBy: String,
+  os: {type: String, trim: true},
+  owner: {type: String, trim: true},
+  patchDate: {type: String, trim: true},
+  serverName: {
+    type: String,
+    required: true,
+    unique: true,
+    uniqueCaseInsensitive: true,
+    index: true,
+    trim: true
+  },
+  updatedBy: {type: String, trim: true},
   virtualization: {
     type: String,
     enum: ['physical', 'virtual', 'cloud']
   },
-  vlan: String
+  vlan: {type: String, trim: true}
 });
-
+serverSchema.plugin(uniqueValidator);
 const Server = mongoose.model('Server', serverSchema);
 
 servers.get('/', (req, res, next) => {
@@ -43,9 +51,16 @@ servers.post('/', (req, res, next) => {
       return;
     }
 
-    res.send(newServer);
+    // Duplicate response from Mongo
+    const data = {...newServer}._doc;
+
+    // Remove database specific keys from duplication
+    delete data.__v;
+    delete data._id;
+
+    res.send(data);
   });
 });
-servers.all('/', (req, res, next) => res.set('Allow', 'GET').status(405).end());
+servers.all('/', (req, res, next) => res.set('Allow', 'GET, POST').status(405).end());
 
 module.exports = servers;
