e5d8d8a9cb946e9929519d397c3efa4c79cb61c2
treemmett <treemmett@gmail.com>
2018-06-21 15:27:34 -0600

    Replaces sidebar indicator with tabs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,8 +5,8 @@ import './Sidebar.scss';
 const Sidebar = props => (
   <div className="sidebar">
     <Link to="/" className="brand">netlist</Link>
-    <NavLink to="/servers">Servers</NavLink>
-    <NavLink to="/namekey">Name Key</NavLink>
+    <NavLink to="/servers"><div className="border"/>Servers</NavLink>
+    <NavLink to="/namekey"><div className="border"/>Name Key</NavLink>
   </div>
 );
 
