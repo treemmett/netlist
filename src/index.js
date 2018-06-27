18fd253b2bc2b4596da4dca1cf7e54e0d1ebbfaf
treemmett <treemmett@gmail.com>
2018-06-26 20:14:17 -0600

    Redirects to login if not authenticated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Redirect, Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Toaster } from './components/Toast';
+import history from './utils/history';
+import jwt from 'jsonwebtoken';
+import toast, { Toaster } from './components/Toast';
 
 // Global styles
 import './index.scss';
@@ -21,24 +23,46 @@ axios.defaults.baseURL = '/netlist/api';
 axios.interceptors.response.use(response => {
   // Cache auth token
   if(response.headers['x-auth-token']){
-    localStorage.setItem('authtoken', response.headers['x-auth-token'])
+    localStorage.setItem('authToken', response.headers['x-auth-token'])
   }
   return response;
 });
 
-const Render = () => (
-  <Router>
+const PrivateRoute = props => {
+  let tokenValid = false;
+
+  // Check if token exists
+  if(localStorage.authToken){
+    const token = jwt.decode(localStorage.authToken);
+    const epoch = Math.floor(Date.now() / 1000);
+
+    // Check if token is still valid and not expired
+    tokenValid = token.exp > epoch;
+  }
+
+  if(!tokenValid){
+    toast('Session expired. Please login');
+  }
+  
+
+  return tokenValid
+    ?
     <React.Fragment>
       <Sidebar/>
+      <Route {...props}/>
+    </React.Fragment>
+    :
+    <Redirect to={{pathname: '/login', state: {referrer: props.path}}}/>
+}
 
+const Render = () => (
+  <Router history={history}>
+    <React.Fragment>
       <Switch>
-        <Route exact path="/" component={Console}/>
-
-        <Route exact path="/servers" component={ServerList}/>
-
-        <Route exact path="/namekey" component={NameKey}/>
-
-        <Route exact path="/users" component={Users}/>
+        <PrivateRoute exact path="/" component={Console}/>
+        <PrivateRoute exact path="/servers" component={ServerList}/>
+        <PrivateRoute exact path="/namekey" component={NameKey}/>
+        <PrivateRoute exact path="/users" component={Users}/>
 
         <Route exact path="/login" component={Login}/>
       </Switch>
