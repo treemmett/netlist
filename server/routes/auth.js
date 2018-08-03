5422097bcc6f6ed67faf7892e0892cfea5ad1cb9
treemmett <treemmett@gmail.com>
2018-08-03 16:42:41 -0600

    Adds LDAP authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const auth = require('express').Router();
 const User = require('./users').schema;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const ldap = require('ldapjs');
 
 // Load configuration
 let config;
@@ -12,50 +13,108 @@ try{
 }
 
 auth.post('/', (req, res, next) => {
-  // Get hash from database
-  User.findOne({username: req.body.username}, {hash: 1}, (err, data) => {
+  // Throw if username or password wasn't sent
+  if(!req.body.username || !req.body.password){
+    res.status(401).send({error: ['Username or password is incorrect']});
+    return;
+  }
+
+  const opts = {
+    filter: `(&(objectclass=user)(samaccountname=${req.body.username}))`,
+    scope: 'sub',
+    attributes: ['dn', 'memberOf', 'sAMAccountName']
+  }
+
+  // Connect to LDAP
+  const client = ldap.createClient({
+    url: config.ldap.url,
+    timeout: 5000,
+    connectTimeout: 10000
+  });
+
+  client.bind(config.ldap.username, config.ldap.password, err => {
     if(err){
+      client.unbind();
+
       return next(err);
     }
 
-    // Throw if user doesn't exist
-    if(!data || !req.body.password){
-      res.status(401).send({error: ['Username or password is incorrect']});
-      return;
-    }
+    client.search(config.ldap.cn, opts, (err, search) => {
+      let foundObject = null;
 
-    // Compare hash against password
-    bcrypt.compare(req.body.password, data.hash, (err, match) => {
-      if(err){
-        return next(err);
-      }
+      search.on('searchEntry', entry => {
+        // Stop searching if we found a valid object
+        if(entry.object){
+          foundObject = entry.object;
+          client.unbind();
+        }
+      });
 
-      // Throw if password is incorrect
-      if(!match){
-        res.status(401).send({error: ['Username or password is incorrect']});
-        return;
-      }
+      search.on('error', error => {
+        client.unbind();
+        return next(error);
+      });
 
-      // Update database with login date
-      const epoch = Math.floor(Date.now() / 1000);
-      User.findOneAndUpdate({username: req.body.username}, {lastLogin: epoch}, (err, data) => {
-        if(err){
-          return next(err);
+      search.on('end', result => {
+        client.unbind();
+        
+        // End if no results were found
+        if(!foundObject){
+          res.status(401).send({error: ['Username or password is incorrect']});
+          return;
         }
 
-        // Create a signed token
-        jwt.sign({
-          username: data.username,
-          admin: data.admin
-        }, config.token.secret, {
-          expiresIn: config.token.expiresIn
-        }, (err, token) => {
+        // Create a new connection for user credentials
+        const userClient = ldap.createClient({
+          url: config.ldap.url,
+          timeout: 5000,
+          connectTimeout: 10000
+        });
+        console.log(foundObject.dn);
+        userClient.bind(foundObject.dn, req.body.password, err => {
           if(err){
+            // Check if error happened due to password error
+            if(err.name === 'InvalidCredentialsError'){
+              res.status(401).send({error: ['Username or password is incorrect']});
+              return;
+            }
+
             return next(err);
           }
 
-          res.set('X-Auth-Token', token).end();
+          // Add fake members array if it doesn't exist
+          if(typeof foundObject.memberOf !== 'object' && !(foundObject.memberOf instanceof Array)){
+            foundObject.memberOf = [];
+          }
+        
+          // User authentication succeeded. Now check if user has the necessary groups
+          const regRO = new RegExp('^cn='+config.ldap.groups.ro, 'i');
+          const regRW = new RegExp('^cn='+config.ldap.groups.rw, 'i');
+
+          const accessGroups = foundObject.memberOf.filter(group => regRO.test(group) || regRW.test(group));
+
+          if(!accessGroups.length){
+            // User authentication suceeded, but doesn't have permissions
+            res.status(403).send({error: ['You do not have permission to access this page.']});
+            return;
+          }
+
+          // Create signed token
+          jwt.sign({
+            username: foundObject.sAMAccountName,
+            admin: accessGroups.findIndex(group => regRW.test(group)) > -1
+          },
+          config.token.secret,
+          {
+            expiresIn: config.token.expiresIn
+          },
+          (err, token) => {
+            if(err) return next(err);
+
+            res.set('X-Auth-Token', token).end();
+          });
         });
+
       });
     });
   });
