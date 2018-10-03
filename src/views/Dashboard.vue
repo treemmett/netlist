e404bb368606c5d6ea01679665647eb39932cb0f
treemmett <tregan@tregan.me>
2018-10-03 16:47:13 -0600

    Adds dynamic details pane

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="wrapper">
-
+  <div class="wrapper" v-bind:class="{open: detailsOpen}">
     <div class="nav">
       <div class="brand">Netlist</div>
 
@@ -21,31 +20,104 @@
       <div class="table">
         <table>
           <thead>
-            <tr><th>Column</th><th>Column</th><th>Column</th><th>Column</th></tr>
+            <tr>
+              <th>Name</th>
+              <th>Location</th>
+              <th>URL</th>
+            </tr>
           </thead>
           <tbody>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
-            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr v-for="item in tableData" :key="item.id" v-on:click="openDetails(item)" v-bind:class="{selected: item.id === selectedItem && detailsOpen}">
+              <td>{{item.name}}</td>
+              <td>{{item.location}}</td>
+              <td>{{item.url}}</td>
+            </tr>
           </tbody>
         </table>
       </div>
     </div>
 
-    <div class="details"/>
+    <div class="details">
+      <div class="menu">
+        <!-- close button -->
+        <div class="icon" v-on:click="closeDetails">
+          <svg viewBox="0 0 24 24">
+            <line x1="6" y1="6" x2="18" y2="18"/>
+            <line x1="18" y1="6" x2="6" y2="18"/>
+          </svg>
+        </div>
+
+        <div class="name">{{currentItem.name}}</div>
+
+        <div class="right">
+          <!-- edit button -->
+          <div class="icon">
+            <svg viewBox="0 0 24 24">
+              <polygon points="16 3 21 8 8 21 3 21 3 16 16 3"/>
+            </svg>
+          </div>
+          
+          <!-- delete button -->
+          <div class="icon">
+            <svg viewBox="0 0 24 24">
+              <polyline points="3 6 5 6 21 6"/>
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      detailsOpen: false,
+      selectedItem: undefined,
+      tableData: [
+        {
+          id: '1226221511',
+          name: 'DCA21001',
+          location: 'Denver',
+          url: 'dca21001.corp.net'
+        },
+        {
+          id: '2652277226',
+          name: 'DCA21002',
+          location: 'Portland',
+          url: 'dca21002.corp.net'
+        },
+        {
+          id: '7479384320',
+          name: 'DCA21003',
+          location: 'Pleasant Grove',
+          url: 'dca21003.corp.net'
+        }
+      ]
+    }
+  },
+  methods: {
+    closeDetails(){
+      this.detailsOpen = false;
+    },
+
+    openDetails(e){
+      this.detailsOpen = true;
+      this.selectedItem = e.id;
+    }
+  },
+  computed: {
+    currentItem(){
+      // Find item that's currently selected
+      return this.tableData.find(i => i.id === this.selectedItem) || {};
+    }
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
   $blue: #4183f8;
@@ -60,9 +132,9 @@
   }
 
   .wrapper.open{
-    .table{
+    .main{
       flex: 0;
-      flex-basis: 20em;
+      flex-basis: 45em;
 
       @media screen and (max-widtd: 1000px){
         flex-basis: 0;
@@ -103,7 +175,7 @@
         cursor: pointer;
 
         &:hover{
-          background-color: rgba($blue, 0.1);
+          background-color: rgba(#000, 0.05);
         }
 
         &.router-link-exact-active{
@@ -154,15 +226,16 @@
         &:nth-child(2n){
           background-color: #f5f5f5;
         }
+
+        &.selected{
+          background-color: rgba($blue, 0.2);
+          color: $blue;
+        }
       }
 
       td, th{
         padding: 1em;
-        text-align: right;
-
-        &:first-child{
-          text-align: left;
-        }
+        text-align: left;
       }
 
       th{
@@ -174,8 +247,49 @@
   }
 
   .details{
-    background-color: cyan;
     transition: flex-grow 0.4s ease-in-out;
     flex-grow: 0;
+    min-width: 0;
+    overflow: hidden;
+
+    .menu{
+      padding: 2em 1em;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .icon{
+        width: 30px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        transition: background-color 0.1s ease-out;
+        cursor: pointer;
+
+        &:hover{
+          background-color: hsl(0, 0%, 95%);
+        }
+
+        svg{
+          width: 24px;
+          height: 24px;
+          stroke-linejoin: round;
+          stroke-linecap: round;
+          stroke-width: 2;
+          fill: none;
+          stroke: #666;
+        }
+      }
+
+      .right{
+        display: flex;
+
+        .icon{
+          margin-left: 1em;
+        }
+      }
+    }
   }
 </style>
