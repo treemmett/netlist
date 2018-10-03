0c34658c3a6bec9b5536cf2d2034004452a2ba4f
treemmett <tregan@tregan.me>
2018-10-03 12:44:43 -0600

    Adds table layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper open">
+  <div class="wrapper">
 
     <div class="nav">
       <div class="brand">Netlist</div>
@@ -13,9 +13,36 @@
       </div>
     </div>
 
-    <div class="table"/>
+    <div class="main">
+      <div class="actions">
+        <input type="search" placeholder="Search"/>
+      </div>
 
-    <div class="main"/>
+      <div class="table">
+        <table>
+          <thead>
+            <tr><th>Column</th><th>Column</th><th>Column</th><th>Column</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+            <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="details"/>
 
   </div>
 </template>
@@ -37,12 +64,12 @@
       flex: 0;
       flex-basis: 20em;
 
-      @media screen and (max-width: 1000px){
+      @media screen and (max-widtd: 1000px){
         flex-basis: 0;
       }
     }
 
-    .main{
+    .details{
       flex-grow: 1;
     }
   }
@@ -53,7 +80,6 @@
     padding-top: 1em;
     box-sizing: border-box;
     overflow-y: auto;
-    overflow-y: overlay;
 
     .btn{
       font-size: 13px;
@@ -70,7 +96,6 @@
         color: inherit;
         text-decoration: none;
         padding: 0.75em 1em;
-        margin-right: 0.5em;
         border-top-right-radius: 50em;
         border-bottom-right-radius: 50em;
         font-size: 15px;
@@ -89,12 +114,66 @@
     }
   }
 
-  .table{
-    background-color: teal;
+  .main{
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
     transition: flex-basis 0.4s ease-in-out, flex-grow 0.4s ease-in-out;
+
+    .actions{
+      margin-bottom: 1em;
+
+      input[type=search]{
+        appearance: none;
+        padding: 5px 10px;
+        font-size: 14px;
+        border: 1px solid #bbb;
+        border-radius: 5px;
+        outline: none;
+        width: 100%;
+        max-width: 20em;
+
+        &:focus{
+          border-color: #71c0f5;
+          box-shadow: 0 0 1px #71c0f5;
+        }
+      }
+    }
+
+    .table{
+      border-radius: 8px;
+      flex-grow: 1;
+      overflow: auto;
+
+      table{
+        width: 100%;
+        border-collapse: collapse;
+      }
+
+      tr{
+        &:nth-child(2n){
+          background-color: #f5f5f5;
+        }
+      }
+
+      td, th{
+        padding: 1em;
+        text-align: right;
+
+        &:first-child{
+          text-align: left;
+        }
+      }
+
+      th{
+        position: sticky;
+        top: 0;
+        background-color: #f8f8f8;
+      }
+    }
   }
 
-  .main{
+  .details{
     background-color: cyan;
     transition: flex-grow 0.4s ease-in-out;
     flex-grow: 0;
